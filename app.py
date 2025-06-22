c375a6df53dd89b4eaf09fa890ca1e464975412f
Thene12xx <ronahhyo@gmail.com>
2025-06-22 18:51:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,13 @@
 import os
 from flask import Flask, render_template
 
-# Get the directory of the current file
-current_dir = os.path.dirname(os.path.abspath(__file__))
-
-# Set the path to your templates folder
-template_path = os.path.join(current_dir, 'templates')
-
-# Create the Flask app with template_folder specified
-app = Flask(__name__, template_folder=template_path)
+# Set the Flask app and templates folder
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+# This block is ignored by Gunicorn (used only for local testing)
 if __name__ == '__main__':
     app.run(debug=True)
